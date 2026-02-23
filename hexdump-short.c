78ee7156370737955c663bb40ff7b047beace96b
init-zhang <init-zhang@proton.me>
2026-02-23 20:12:00 +0000

    hexdump for byte and short

--- hexdump-short.c
+++ hexdump-short.c
@@ -1,12 +1,15 @@
-/* Hexdumps a file, a byte  at a time
+/* Hexdumps a file, a short (16 bits) at a time
+ * Does not accurately display endianness because hex for 2 bytes is displayed
+ * as big endian after being converted into a short
+ * e.g. 424D is displayed 4D42 in output
  *
- * Only argument is number of bytes to read from file
+ * Only argument is number of shorts to read from file
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define columns 16
+#define columns 8
 
 int main(int argc, char* argv[]) {
     int bufferSize;
@@ -15,7 +18,7 @@ int main(int argc, char* argv[]) {
     } else {
         bufferSize = columns;
     }
-    unsigned char buffer[bufferSize];
+    unsigned short buffer[bufferSize];
     FILE *ptr;
 
     ptr = fopen("demo.bmp","rb");
@@ -24,13 +27,13 @@ int main(int argc, char* argv[]) {
 
     for (int i = 0; i < bufferSize / columns; i++) {
         for (int j = 0; j < columns; j++)
-            printf("%02X ", buffer[i*columns+j]);
+            printf("%04X ", buffer[i*columns+j]);
         printf("\n");
     }
     int roundedDown = bufferSize & ~(columns-1);
     if (bufferSize != roundedDown) {
         for (int i = 0; i < bufferSize % columns; i++)
-            printf("%02X ", buffer[roundedDown + i]);
+            printf("%04X ", buffer[roundedDown + i]);
         printf("\n");
     }
 
