4b90935c7ce49329adb8c9e549e08e88e265f2fb
init-zhang <init-zhang@proton.me>
2026-02-24 11:29:04 +0000

    hexdump byte printRow complete
    * no longer displays every byte pair as one
    * rounds up length to the nearest 16

--- hexdump.c
+++ hexdump.c
@@ -11,27 +11,31 @@
 #define columns 16
 // Can be changed to hexadecimal by replacing d with x
 #define lineNumberFormat "%02d: "
+#define bufferSize 16
 
 /**
  * Prints a row of bytes from a file of length bufferSize
  *
  * @param ptr         file pointer to read from
  * @param buffer      buffer to write file row into
- * @param bufferSize  size of buffer, avoids sizeof call for each row
+ * @param lineNumber  the address of the first byte, used for line numbers
  */
-void printRow(FILE *ptr, int *buffer, int bufferSize) {
-    fread(buffer, bufferSize, 1, ptr)
+void printRow(FILE *ptr, unsigned char *buffer, int lineNumber) {
+    fread(buffer, bufferSize, 1, ptr);
+    printf(lineNumberFormat, lineNumber);
+    for (int i = 0; i < bufferSize; i++)
+        printf("%02X ", buffer[i]);
+    printf("\n");
 }
 
 int main(int argc, char* argv[]) {
-    int bufferSize;
     int startAddress;
-    int endAddress;
+    int length;
 
     if (argc == 2 ) {
-        bufferSize = atoi(argv[1]);
+        length = atoi(argv[1]);
     } else {
-        bufferSize = columns;
+        length = 16;
     }
 
     unsigned char buffer[bufferSize];
@@ -39,21 +43,10 @@ int main(int argc, char* argv[]) {
 
     ptr = fopen("demo.bmp","rb");
 
-    fread(buffer, sizeof(buffer), 1, ptr);
+    int roundedUp = (length + (columns-1)) & ~(columns-1);
 
-    for (int i = 0; i < bufferSize / columns; i++) {
-        printf(lineNumberFormat, i*columns);
-        for (int j = 0; j < columns; j += 2)
-            printf("%02X%02X ", buffer[i*columns+j], buffer[i*columns+j+1]);
-        printf("\n");
-    }
-    int roundedDown = bufferSize & ~(columns-1);
-    if (bufferSize != roundedDown) {
-        printf(lineNumberFormat, roundedDown);
-        for (int i = 0; i < bufferSize % columns; i += 2)
-            printf("%02X%02X ", buffer[roundedDown+i], buffer[roundedDown+i+1]);
-        printf("\n");
-    }
+    for (int i = 0; i < roundedUp; i += columns)
+        printRow(ptr, buffer, i);
 
     return 0;
 }
